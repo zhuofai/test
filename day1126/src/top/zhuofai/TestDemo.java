8264aff585a8896c26ec4dd9a8bd34d37e5d010a
zhuofai <2287233405@qq.com>
2018-11-28 19:06:34 +0800

    hello3dev1

---
+++ day1126/src/top/zhuofai/TestDemo.java
@@ -0,0 +1,13 @@
+package top.zhuofai;
+
+public class TestDemo {
+    public void test(){
+        System.out.println("hello");
+    }
+    public void add(){
+        System.out.println("nihao a");
+    }
+    public void dev(){
+
+    }
+}
